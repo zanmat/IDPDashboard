1325beb13e54cef35490baf977a40da3ea8b930c
zanmat <mattiazanazzi@gmail.com>
2016-11-30 10:54:17 +0300

    revised for two maps, changed colors

--- js/site.js
+++ js/site.js
@@ -4,13 +4,14 @@ var config = {
     title:"Northern Syria CCCM Cluster: IDP Displacements",
     description:"<p>Click the graphs or map to interact.<br />Date: 28/11/2016 <br />Source: CCCM Cluster Member Agencies</p>",
     data:"data/data4.json",
-    whoFieldName:"F_Subdistrict",
+    whoFieldName:"F_Subdistrict", //barsdeparture
     whatFieldName:"Nov_Change",
-    whereFieldName:"T_Subdistrict", //map
-    statusFieldName:"T_Subdistrict", //bars
+    whereFieldName:"T_Subdistrict", //mapdestination
+    whereFieldName2:"F_Subdistrict", //mapdeparture
+    statusFieldName:"T_Subdistrict", //barsdestination
     geo:"data/syria2.geojson",
     joinAttribute:"NAM_EN_REF",
-    colors:['#fdae6b','#fff5f0','#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#99000d']
+    colors:['#edf8e9','#bae4b3','#74c476','#238b45','#fee5d9','#fcae91','#fb6a4a','#cb181d','#99000d']
 };
 
 //function to generate the 3W component
@@ -25,6 +26,7 @@ function generate3WComponent(config,data,geom){
     var whoChart = dc.rowChart('#hdx-3W-who');
     var whatChart = dc.rowChart('#hdx-3W-what');
     var whereChart = dc.leafletChoroplethChart('#hdx-3W-where');
+    var whereChart2 = dc.leafletChoroplethChart('#hdx-3W-where2');
     var statusChart = dc.rowChart('#hdx-3W-status');
 
     var cf = crossfilter(data);
@@ -32,25 +34,27 @@ function generate3WComponent(config,data,geom){
     var whoDimension = cf.dimension(function(d){ return d[config.whoFieldName]; });
     var whatDimension = cf.dimension(function(d){ return d[config.whatFieldName]; });
     var whereDimension = cf.dimension(function(d){ return d[config.whereFieldName]; });
+    var whereDimension2 = cf.dimension(function(d){ return d[config.whereFieldName2]; });
     var statusDimension = cf.dimension(function(d){ return d[config.statusFieldName]; });
 
     var whoGroup = whoDimension.group().reduceSum(function(d) {return d.Nov_Change ;});
     var whatGroup = whatDimension.group();
     var whereGroup = whereDimension.group().reduceSum(function(d) {return d.Nov_Change ;});
+    var whereGroup2 = whereDimension2.group().reduceSum(function(d) {return d.Nov_Change ;});
     var statusGroup = statusDimension.group().reduceSum(function(d) {return d.Nov_Change ;});
     var all = cf.groupAll();
 
-    whoChart.width($('#hdx-3W-who').width()).height(440)
+    whoChart.width($('#hdx-3W-who').width()).height(200)
             .dimension(whoDimension)
             .group(whoGroup)
             .elasticX(true)
             .data(function(group) {
-                return group.top(20);
+                return group.top(5);
             })
             .labelOffsetY(13)
             .colors(config.colors)
             .colorDomain([0,7])
-            .colorAccessor(function(d, i){return 0;})
+            .colorAccessor(function(d, i){return 2;})
             .xAxis().ticks(5);
 
     whatChart.width($('#hdx-3W-what').width()).height(250)
@@ -66,12 +70,12 @@ function generate3WComponent(config,data,geom){
             .colorAccessor(function(d, i){return 0;})
             .xAxis().ticks(5);
     
-    statusChart.width($('#hdx-3W-status').width()).height(510)
+    statusChart.width($('#hdx-3W-status').width()).height(200)
             .dimension(statusDimension)
             .group(statusGroup)
             .elasticX(true)
             .data(function(group) {
-                return group.top(15);
+                return group.top(5);
             })    
             .labelOffsetY(13)
             .colors(config.colors)
@@ -82,17 +86,20 @@ function generate3WComponent(config,data,geom){
     dc.dataCount('#count-info')
             .dimension(cf)
             .group(all);
-
-    whereChart.width($('#hxd-3W-where').width()).height(400)
-            .dimension(whereDimension)
-            .group(whereGroup)
-            .center([36, 38])
+    
+     
+    whereChart2.width($('#hxd-3W-where2').width()).height(100)
+            .dimension(whereDimension2)
+            .group(whereGroup2)
+            .center([35.8, 38])
             .zoom(7)    
             .geojson(geom)
-            .colors(['#CCCCCC', config.colors[1],config.colors[3], config.colors[6]])
-            .colorDomain([0, 1, 2, 3])
+            .colors(['#CCCCCC', config.colors[0],config.colors[1], config.colors[2], config.colors[3]])
+            .colorDomain([0, 1, 2, 3, 4])
             .colorAccessor(function (d) {
-                if(d>3000){
+                if(d>10000){
+                    return 4;
+                } else if (d>3000) {
                     return 3;
                 } else if (d>1000) {
                     return 2;
@@ -106,7 +113,35 @@ function generate3WComponent(config,data,geom){
             .featureKeyAccessor(function(feature){
                 return feature.properties[config.joinAttribute];
             });
-                                
+    
+    whereChart.width($('#hxd-3W-where').width()).height(100)
+            .dimension(whereDimension)
+            .group(whereGroup)
+            .center([35.8, 38])
+            .zoom(7)    
+            .geojson(geom)
+            .colors(['#CCCCCC', config.colors[4],config.colors[5], config.colors[6], config.colors[7]])
+            .colorDomain([0, 1, 2, 3, 4])
+            .colorAccessor(function (d) {
+                if(d>4000){
+                    return 4;
+                } else if (d>1000) {
+                    return 3;
+                } else if (d>500) {
+                    return 2;
+                }
+                else if (d>1) {
+                    return 1;
+                }
+                    else {
+                    return 0;
+                }
+            })           
+            .featureKeyAccessor(function(feature){
+                return feature.properties[config.joinAttribute];
+            });
+     
+   
     dc.renderAll();
     
     var g = d3.selectAll('#hdx-3W-who').select('svg').append('g');
